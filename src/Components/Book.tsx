4a4c7d405981b3bcd000befd0e1d0b329163ce18
notPlancha <andre.plancha@hotmail.com>
2024-04-09 23:59:13 +0100

    started dropdown, not working currently

--- src/Components/Book.tsx
+++ src/Components/Book.tsx
@@ -6,16 +6,17 @@ function Book() {
 
 // há 1 livro de teste, TODO dps perguntar ao prof o q fazer com esse
 export interface Book {
-  id: `${number}` | string // livro de teste é 120a
+  id: `${number}`
   title: string
   isbn?: string
-  pageCount?: number // teste n tem tmb
-  publishedDate?: {$date: `${(number)}-${(number)}-${(number)}`} // YYYY-MM-DD
+  pageCount: number
+  publishedDate: {$date: `${(number)}-${(number)}-${(number)}`} // YYYY-MM-DD
   thumbnailUrl?: string
   shortDescription?: string
   longDescription?: string
-  status?: "PUBLISH"
-  authors?: string[]
-  categories?: string[]
-  score?: number
+  status: "PUBLISH"
+  authors: string[]
+  categories: string[]
+  score: number
+  price?: number
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,20 +1,44 @@
-import {Button, Navbar} from "react-bulma-components";
+// https://react-restart.github.io/ui/
+import {Nav, NavItem as ReNavItem, Button, NavItemProps, Dropdown} from "@restart/ui";
 
+import {DynamicRefForwardingComponent} from "@restart/ui/types";
+import {FC, useState} from "react";
+import {UseDropdownToggleProps} from "@restart/ui/DropdownToggle";
+import {UserDropdownMenuProps} from "@restart/ui/DropdownMenu";
+
+const NavItem: FC<NavItemProps> = ({children, ...props}) => {
+  return <ReNavItem as={"a"} {...props}>{children}</ReNavItem>
+}
+// TODO add burger https://bulma.io/documentation/components/navbar/
 function Header() {
+
+  const [dropdownShow, setDropdownShow] = useState(true)
+
   return (
-    <Navbar px={2}>
-      <Navbar.Brand>
-        <Navbar.Item href={"/"}>
+    <Nav className={"navbar"} role={"navigation"}>
+      <div className={"navbar-brand"}>
+        <NavItem className={"navbar-item"}>
           <img src={"/vite.svg"} alt={"logo"}/>
-        </Navbar.Item>
-      </Navbar.Brand>
-      <Navbar.Container align={"left"}>
-
-      </Navbar.Container>
-      <Navbar.Container align={"right"}>
-
-      </Navbar.Container>
-    </Navbar>
+        </NavItem>
+      </div>
+      <div className={"navbar-menu"} id={"header-menu"}>
+        <div className={"navbar-start"}>
+          <NavItem className={"navbar-item has-dropdown"}>
+            <Dropdown show={false} onToggle={(nextShow) => setDropdownShow(nextShow)} placement={"bottom-end"}>
+              <Dropdown.Toggle>
+                {(props: UseDropdownToggleProps) => <a className={"navbar-link"}>Discover</a>}
+              </Dropdown.Toggle>
+              <Dropdown.Menu usePopper={false}>
+                {(props: UserDropdownMenuProps) => (<a className={dropdownShow ? "navbar-dropdown is-active" : "navbar-dropdown"}>Dropdown</a>)}
+              </Dropdown.Menu>
+            </Dropdown>
+          </NavItem>
+        </div>
+        <div className={"navbar-end"}>
+          {/*TODO shopping cart*/}
+        </div>
+      </div>
+    </Nav>
   )
 }
 
