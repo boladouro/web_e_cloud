b8604ef1ebe31529fc4acf0375722f1e55b92051
notPlancha <andre.plancha@hotmail.com>
2024-04-10 22:54:18 +0100

    header done, no linking yet

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import { Form } from "react-router-dom";
 import styled from 'styled-components';
 import { FaSearch } from 'react-icons/fa'; // Import the Font Awesome icon
+import { BulmaSize } from "../entry.tsx";
 
-const SearchIcon = styled(FaSearch)`
+
+
+export function SearchBar({size = "medium", className = ""}:{size: BulmaSize, className?: string}) {
+  const SearchIcon = styled(FaSearch)`
   position: absolute;
-  top: 0.75em;
+  top: 32%;
   right: 0.75em;
   z-index: 4;
   color: var(--bulma-input-placeholder-color);
-  font-size:  var(--bulma-size-medium);
+  font-size: var(--bulma-size-${size});
 `;
-
-export function SearchBar({className = ""}:{className?: string}) {
   return (
     <div className="field has-addons">
       <div className="control">
-        <input className={"input is-medium " + className} type="text" placeholder="Book"/>
+        <input className={`input is-${size} ${className}`} type="text" placeholder="Book"/>
         <SearchIcon />
       </div>
     </div>
--- src/entry.tsx
+++ src/entry.tsx
@@ -36,3 +36,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router}  />
   </React.StrictMode>,
 )
+
+
+export type BulmaSize = "small" | "normal" | "medium" | "large"
