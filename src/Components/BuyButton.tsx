8e3f2a532b9c78527fd50f127cc929cf41598e2c
kardecallan566 <kardecallan56@gmail.com>
2024-04-18 20:35:06 +0100

    cart feito, falta melhorar css

--- src/Components/BuyButton.tsx
+++ src/Components/BuyButton.tsx
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 import { FaCartPlus } from 'react-icons/fa';
 import { BulmaSize } from "../types/types.ts";
 import CartContext from "../entry.tsx"; 
+import {Link} from "react-router-dom";
 
 
 export function Buybutton({ size = "medium", book, }: { size: BulmaSize, book:Book, className?: string }) {
 
+  const {addCart , Books,NBook } = useContext(CartContext); 
 
   const BuyIcon = styled(FaCartPlus)`
     top: 32%;
@@ -17,10 +19,21 @@ export function Buybutton({ size = "medium", book, }: { size: BulmaSize, book:Bo
     font-size: var(--bulma-size-${size});
   `;
 
+  const handleAddToCart = () => {
+    console.log("books", book)
+    addCart(book);
+  };
 
+  console.log(Books,"Books")
+  console.log(NBook,"NBooks")
   return (
     <div className="flex-justify-space-evenly">
-      <a href="/cart"><BuyIcon /></a>
+       <button onClick={handleAddToCart}> {/* Use um botão em vez de um link */}
+        <BuyIcon />
+      </button>
+      <Link to={`/cart`} className={"p-4 w10 h2xl"}>
+        <p> clica aquui para acessar a lojinha</p>
+      </Link>
     </div>
   );
 }
--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 import React, { useContext, useEffect } from "react";
 import CartContext from "../entry.tsx";
 import Slider, { Settings as SliderSettings } from "react-slick";
+import { styled } from "styled-components";
+import {Book} from "../types/types.ts";
+import BookComponent from "../Components/BookComponent.tsx";
 
 
-export default function BookComponent() {
-    const sliderSettings: SliderSettings = {
-        centerMode: true,
-        infinite: true,
-        slidesToShow: 3,
-        variableWidth: true,
-        speed: 300,
-        adaptiveHeight: false,
-        dots: false,
-    }
-    const StyledSlider = styled(Slider)`
+export default function CartComponent() {
+  const sliderSettings: SliderSettings = {
+    centerMode: true,
+    infinite: false,
+    slidesToShow: 1,
+    variableWidth: true,
+    speed: 300,
+    adaptiveHeight: false,
+    dots: false,
+  }
+  const StyledSlider = styled(Slider)`
     .slick-prev::before, .slick-next::before {
       font-size: 30px !important;
     }
@@ -32,18 +35,16 @@ export default function BookComponent() {
 
 
 
-    const { Total, NBook, Books } = useContext(CartContext);
+  const { Total, NBook, Books } = useContext(CartContext);
+  console.log(Books,"book")
 
+  return <>
+    <div className={"p-t1vh p-l4 p-r4"}>
+      <h1>Cart</h1>
+          <StyledSlider className={"mxxl"} {...sliderSettings}>
+            {Books.map((book: Book ) => <BookComponent  key={book.id} book={book}/>)}
+          </StyledSlider>
+    </div>
 
-    return <>
-            <div className={"p-t1vh p-l4 p-r4"}>
-                <h1>Cart</h1>
-                <StyledSlider className={"mxxl"} {...sliderSettings}>
-                    {Books.map((Book) => (
-                    <Link to={`/book/${Book.id}`} key={Book.id} className={"p-4 w10 h2xl"}>
-                        <></>
-                    </Link>
-                    ))}
-                </StyledSlider>
-            </div>
-    </>}
+  </>
+}
