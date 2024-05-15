7d90bf6e48c046e402bb28328b997a44501ce39e
notPlancha <andre.plancha@hotmail.com>
2024-05-16 00:36:35 +0100

    jsonprovider now compatible

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,34 @@
 from flask import Flask, jsonify
+from flask.json.provider import JSONProvider
 from pymongo import MongoClient
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from bson import ObjectId, json_util
 
 load_dotenv()
 connection_string = os.getenv("MONGODB_CONECTION_STRING")
 if not connection_string:
   raise ValueError("No connection string found, create a .env file with the MONGODB_CONECTION_STRING variable.")
 
+
+class MongoJSONProvider(JSONProvider):
+  def __init__(self, *args, **kwargs):
+    super(MongoJSONProvider, self).__init__(*args, **kwargs)
+
+  def dumps(self, obj, **kwargs):
+    return json_util.dumps(obj, **kwargs)
+  def loads(self, s, **kwargs):
+    return json_util.loads(s, **kwargs)
+
+
 app = Flask(__name__)
+app.json_provider_class = MongoJSONProvider
+app.json = MongoJSONProvider(app)
 CORS(app)
 client = MongoClient(
   connection_string,
-  tls=True,
+  tls=os.getenv("MONGODB_TLS", True) in ("1", "true", "True", "yes", "Yes", "y", "Y"),
   tlsAllowInvalidCertificates=True,
   timeoutMS=5000,
 )
@@ -21,6 +36,7 @@ db = client["backend"]
 if "books" not in db.list_collection_names():
   ValueError("No books collection found, run `pixi run setup-db` or use mongoimport to create it")
 
-@app.route("/books", methods=["GET"])
+
+@app.route("/api/v1/books", methods=["GET"])
 def get_books():
-  return jsonify([]), 200
+  return list(db.books.find({}))
